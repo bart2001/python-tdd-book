8d85160c543fdff09c6f177da86be015515a762b
bart2001 <bart2001@naver.com>
2019-12-31 15:57:23 +0900

    liveviewtest added

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,5 +18,5 @@ from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    #url(r'front_test', views.front_test, name='front_test')
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
